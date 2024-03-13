98c5b5f960d2793b194df4d9ba3ee2b4f9a5aab5
Isaac <morabisaac@gmail.com>
2024-03-12 21:05:45 -0600

    semana 9

---
+++ src/main/java/com/tienda2/controller/ProductoController.java
@@ -0,0 +1,75 @@
+package com.tienda2.controller;
+
+import com.tienda2.domain.Producto;
+import com.tienda2.service.CategoriaService;
+import com.tienda2.service.ProductoService;
+import com.tienda2.serviceimpl.FirebaseStorageServiceImpl;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author isaac
+ */
+@Controller
+@RequestMapping("/producto")
+public class ProductoController {
+    
+    @Autowired
+    ProductoService productoService;
+    
+    @GetMapping("/listado")
+    public String page(Model model) {
+        List <Producto> lista = productoService.getProductos(false);
+        model.addAttribute("productos", lista);
+        model.addAttribute("totalProductos", lista.size());
+        model.addAttribute("productos", CategoriaService.getCategoria(true));
+        return "/producto/listado";
+    }
+    
+    @GetMapping("/nuevo")
+    public String productoNuevo(Producto producto) {
+        return "/producto/modifica";
+    }
+
+    @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
+    
+    @PostMapping("/guardar")
+    public String productoGuardar(Producto producto,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {        
+        if (!imagenFile.isEmpty()) {
+            productoService.save(producto);
+            producto.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile, 
+                            "producto", 
+                            producto.getIdProducto()));
+        }
+        productoService.save(producto);
+        return "redirect:/producto/listado";
+    }
+
+    @GetMapping("/eliminar/{idProducto}")
+    public String productoEliminar(Producto producto) {
+        productoService.delete(producto);
+        return "redirect:/producto/listado";
+    }
+
+    @GetMapping("/modificar/{idProducto}")
+    public String productoModificar(Producto producto, Model model) {
+        producto = productoService.getProducto(producto);
+        model.addAttribute("producto", producto);
+        model.addAttribute("productos", productoService.getProductos(true));
+        return "/producto/modifica";
+    }
+}
+    
+
